5be162965d1d0faf6e0c1de3384c2a2a9951a001
mathew boyles <hello@mathewboyles.com>
2017-09-08 09:53:50 +1200

    Search functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,55 @@
 var config, token;
+var pauseUpdate = false;
 
 $(document).ready(function() {
 
-    $.getJSON("/config.json", function(data){
+    $.getJSON("/config.json", function(data) {
         config = data;
         init();
     });
 
-    $("#sidebar-new").click(function(){
-        if(!token) {
+    $("#search-input").on("input change focus blur", function() {
+        pauseUpdate = !!$(this).val();
+
+        var searchTerm = $(this).val().toLowerCase();
+
+        $("#posts .card .search").each(function() {
+            $(this).before($(this).html());
+            $(this).remove();
+        });
+
+        if (!searchTerm) return;
+        $("#posts .card").filter(function() {
+            return (($(this).find("h4.card-title").html().toLowerCase().indexOf(searchTerm) > -1) || ($(this).find(".card-body div:first").html().toLowerCase().indexOf(searchTerm) > -1));
+        }).each(function() {
+            var wrapStart = "<span class=\"search\">";
+            var wrapEnd = "</span>";
+
+            var titleIndex = $(this).find("h4.card-title").html().toLowerCase().indexOf(searchTerm);
+            if (titleIndex > -1) {
+                var title = $(this).find("h4.card-title").html();
+                var oTitle = title;
+                var titleSearch = oTitle.substr(titleIndex, searchTerm.length);
+                title = oTitle.substr(0, titleIndex) + wrapStart + titleSearch + wrapEnd;
+                title += oTitle.substr(titleIndex + (searchTerm.length));
+                $(this).find("h4.card-title").html(title);
+            }
+
+            var bodyIndex = $(this).find(".card-body div:first").html().toLowerCase().indexOf(searchTerm);
+            if (bodyIndex > -1) {
+                var body = $(this).find(".card-body div:first").html();
+                var oBody = body;
+                var bodySearch = oBody.substr(bodyIndex, searchTerm.length);
+                body = oBody.substr(0, bodyIndex) + wrapStart + bodySearch + wrapEnd;
+                body += oBody.substr(bodyIndex + (searchTerm.length));
+
+                $(this).find(".card-body div:first").html(body);
+            }
+        });
+    });
+
+    $("#sidebar-new").click(function() {
+        if (!token) {
             window.location = "/auth";
             return;
         }
@@ -16,23 +57,23 @@ $(document).ready(function() {
         $("#new-post input:first").focus();
     });
 
-    $("textarea").on("input", function(){
+    $("textarea").on("input", function() {
         $(this).height(0);
         $(this).height($(this)[0].scrollHeight);
     });
 
-    $("#cancel-post").click(function(){
+    $("#cancel-post").click(function() {
         $("#new-post").slideUp();
     });
 
     $("#create-post [required]").removeAttr("required").attr("data-required", "1");
 
-    $("#create-post").submit(function(event){
+    $("#create-post").submit(function(event) {
         event.preventDefault();
 
-        if($(":input[data-required]").filter(function(){
-            return !$(this).val();
-        }).length) {
+        if ($(":input[data-required]").filter(function() {
+                return !$(this).val();
+            }).length) {
             $("#post-error").html("Please enter all required fields.").show();
         } else {
             $("#post-error").hide();
@@ -44,7 +85,7 @@ $(document).ready(function() {
                     content: $("#post-text").val(),
                     token: token
                 },
-                success: function(data){
+                success: function(data) {
                     console.log(data);
                     $("#new-post").slideUp();
                     $("#create-post")[0].reset();
@@ -55,15 +96,15 @@ $(document).ready(function() {
         }
     });
 
-    if(parseQueryString()["code"]) {
-        setTimeout(function(){
+    if (parseQueryString()["code"]) {
+        setTimeout(function() {
             $.ajax({
                 url: "/gettoken",
                 data: {
                     code: parseQueryString()["code"]
                 },
-                success: function(data){
-                    if(data == "error") alert("Auth error");
+                success: function(data) {
+                    if (data == "error") alert("Auth error");
                     else {
                         token = data;
                         sessionStorage.setItem("batsblog__token", data);
@@ -78,21 +119,23 @@ $(document).ready(function() {
     }
 });
 
-function init(){
+function init() {
     loadPosts();
 
     setInterval(loadPosts, 30000);
 }
 
-function loadPosts(){
+function loadPosts() {
+    if (pauseUpdate) return;
+
     $.ajax({
-        url: "https://www.googleapis.com/blogger/v3/blogs/"+config.blogID+"/posts",
+        url: "https://www.googleapis.com/blogger/v3/blogs/" + config.blogID + "/posts",
         data: {
             key: config.apiKey
         },
-        success: function(data){
+        success: function(data) {
             $("#posts").empty();
-            $.each(data.items, function(i, post){
+            $.each(data.items, function(i, post) {
 
                 var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
                 var lastUpdatedDate = new Date(post.updated);
@@ -103,21 +146,21 @@ function loadPosts(){
                     .addClass("card")
                     .append("<div>")
                     .find("div:last")
-                        .addClass("card-body")
-                        .append("<h4>")
-                        .find("h4:last")
-                            .addClass("card-title")
-                            .html(post.title)
-                            .parent()
-                        .append("<div>")
-                        .find("div:last")
-                            .html(post.content)
-                            .parent()
-                        .append("<hr />")
-                        .append("<small>")
-                        .find("small:last")
-                            .addClass("text-muted")
-                            .html("Last updated: " + lastUpdated + " by <a href=\"" + post.author.url + "\" target=\"_blank\">" + post.author.displayName + "</a>");
+                    .addClass("card-body")
+                    .append("<h4>")
+                    .find("h4:last")
+                    .addClass("card-title")
+                    .html(post.title)
+                    .parent()
+                    .append("<div>")
+                    .find("div:last")
+                    .html(post.content)
+                    .parent()
+                    .append("<hr />")
+                    .append("<small>")
+                    .find("small:last")
+                    .addClass("text-muted")
+                    .html("Last updated: " + lastUpdated + " by <a href=\"" + post.author.url + "\" target=\"_blank\">" + post.author.displayName + "</a>");
                 $("#posts").append($el);
                 delete $el;
             });
